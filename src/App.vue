4c2ecc5ed55e15d924fddc2e13f68ec192353093
Pedro <contato.pedromoc@gmail.com>
2023-03-19 20:24:05 -0300

    Adicionadas imagens à pasta assets, editado component Header

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <template>
-  <LightBoxImage :is-visible="lightBoxOpened" />
   <HeaderComponent />
+  <LightBoxImage :is-visible="lightBoxOpened" />
   <AddToCartButton />
+  <CartComponent />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import HeaderComponent from './components/HeaderComponent.vue'
-import LightBoxImage from './components/LightBoxImage.vue'
+import LightBoxImage from './components/LightBoxImageComponent.vue'
+import CartComponent from './components/cart/CartComponent.vue'
 import AddToCartButton from './components/buttons/AddToCartButton.vue'
 
 export default defineComponent({
   components: {
     HeaderComponent,
     AddToCartButton,
-    LightBoxImage
+    LightBoxImage,
+    CartComponent
   },
   data() {
     return {
---
+++ src/components/cart/CartComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>Cart</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {}
+})
+</script>
+
+<style scoped>
+button {
+  cursor: pointer;
+}
+</style>
